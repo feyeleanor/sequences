f7de6a533840cd2fdcc30403953cee476c202c14
Eleanor McHugh <eleanor@games-with-brains.com>
2012-01-29 12:51:48 +0000

    Added provisional support for ValuesAtOffset(), a multi-value lookup in indexable and slice types

--- indexable.go
+++ indexable.go
@@ -42,3 +42,149 @@ func AtOffset(container interface{}, index int) (r interface{}) {
 	}
 	return
 }
+
+func ValuesAtOffsets(container interface{}, offsets ...int) (r interface{}) {
+	switch container := container.(type) {
+	case Indexable:				n := make([]interface{}, len(offsets))
+								for i, _ := range n {
+									n[i] = container.AtOffset(offsets[i])
+								}
+								r = n
+
+	case []bool:				n := make([]bool, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []interface{}:			n := make([]interface{}, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []int:					n := make([]int, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []int8:				n := make([]int8, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []int16:				n := make([]int16, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []int32:				n := make([]int32, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []int64:				n := make([]int64, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []uint:				n := make([]uint, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []uint8:				n := make([]uint8, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []uint16:				n := make([]uint16, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []uint32:				n := make([]uint32, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []uint64:				n := make([]uint64, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []uintptr:				n := make([]uintptr, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []float32:				n := make([]float32, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []float64:				n := make([]float64, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []complex64:			n := make([]complex64, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []complex128:			n := make([]complex128, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []string:				n := make([]string, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []error:				n := make([]error, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case []R.Value:				n := make([]R.Value, len(offsets))
+								for i, _ := range n {
+									n[i] = container[offsets[i]]
+								}
+								r = n
+
+	case R.Value:				switch container.Kind() {
+								case R.Slice:			panic("implement for arbitrary slices")
+								case R.Map:				panic("implement for arbitrary maps")
+								case R.Func:			panic("implement for arbitrary functions")
+								case R.Chan:			panic("Channels not yet supported")
+								default:				panic("this object cannot be indexed")
+								}
+	default:					switch container := R.ValueOf(container); container.Kind() {
+								case R.Slice:			panic("implement for arbitrary slices")
+								case R.Map:				panic("implement for arbitrary maps")
+								case R.Func:			panic("implement for arbitrary functions")
+								case R.Chan:			panic("Channels not yet supported")
+								default:				panic("this object cannot be indexed")
+								}
+	}
+	return
+}
